05fc329e491ed5c3d470bbae326a399c355f4ac5
randyaajr <randyaajrsp@gmail.com>
2020-12-04 19:51:49 +0000

    Completed app and began design of base.html

--- run.py
+++ run.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template, redirect, url_for
+import os
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+
+@app.route("/services")
+def careers():
+    return render_template("services.html")
+
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
 
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
